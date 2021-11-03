cf57ef11475e8fa9b28090dbbcef260d2549b33a
Julia Kusy <juliakusy@MacBook-Julia.local>
2021-11-03 19:36:07 +0100

    Intialize project

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+/*
+1. Klonowanie repozytorium do siebie
+git clone link_do_repozytorium
+
+2. Sprawdzanie zmian w repozytroium
+git status
+ */
